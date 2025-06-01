6aea95b9c5281a58598e14094f15c1206888d828
waney <ssdii66@gmail.com>
2025-06-01 22:30:32 +0900

    동적으로 파라미터 전달하기

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,17 @@ import About from "./components/About.vue";
 <template>
   <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Dpams Tech</a>
+      <router-link class="navbar-brand" to="/">Dpams Tech</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#title">Home</a>
+            <router-link class="nav-link active" aria-current="page" to="/title">Home</router-link>
           </li>
           <li class="nav-item">
-          <a class="nav-link" href="#portfolio">Portfolio</a>
+          <router-link class="nav-link" to="/portfolio">Portfolio</router-link>
         </li>
         </ul>
       </div>
